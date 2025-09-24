6e06d15e64b5306835a9c6f3959ed3178bc8d2b6
Julian Young <julian.young@cuw.edu>
2025-09-24 14:42:43 -0500

    Update 1.0

--- src/search/MatrixSearch.java
+++ src/search/MatrixSearch.java
@@ -1,5 +1,57 @@
 package search;
 
-public class MatrixSearch {
+public class MatrixSearch
+{
 
+	public static void main(String[] args)
+	{
+		int [][] arr = 
+			{
+					{1, 2, 3, 4},
+					{5, 6, 7, 8},
+					{9, 10, 11, 12}
+			};
+		
+		int[] result = MatrixS(arr, 5);
+		System.out.println("\"Result: [" + result[0] + "," + result[1] + "]");
+		
+	}
+	
+	
+	public class Matrix
+	{
+		public static int[] MatrixS(int[][] arr, int key)
+		{
+			for(int i = 0; i < arr.length; i++) // AI wants us to loop here
+			{
+				int colIndex = binarySearch(arr[i], key);
+				if(colIndex != -1)
+				{
+					return new int[](i, colIndex); // AI belives this is found at row i, column colIndex
+				}
+			}
+			
+			return new int[](-1, -1); // this can't be found
+		}
+	}
+	
+	private static int binarySearch(int[] row, int key) // USING zyBook Section 2.2 for REFERENCE
+	{
+	    int low = 0;
+	    int high = row.length - 1;
+
+	    while (high >= low) {
+	        int mid = (high + low) / 2;
+
+	        if (row[mid] < key) {
+	            low = mid + 1;
+	        } else if (row[mid] > key) {
+	            high = mid - 1;
+	        } else {
+	            return mid;
+	        }
+	    }
+
+	    return -1; // not found
+	}
 }
